0a8e15c1cb49cc2c52ce01bd63ac3ed57c948aed
Sajmir <sajmir.buzi@studio.unibo.it>
2024-08-13 14:30:51 +0200

    modified MaterialComponents.kt

--- app/src/main/java/com/cashflowtracker/miranda/MaterialComponents.kt
+++ app/src/main/java/com/cashflowtracker/miranda/MaterialComponents.kt
@@ -1,39 +1,18 @@
 package com.cashflowtracker.miranda
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Assignment
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Leaderboard
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Schedule
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.rounded.AccountBalance
+import androidx.compose.material.icons.rounded.Assignment
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Leaderboard
+import androidx.compose.material.icons.rounded.Schedule
 import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -41,10 +20,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
-import com.cashflowtracker.miranda.R
+
+// class account
+data class Account(val name: String, val balance: String, val icon: ImageVector, val isFavorite: Boolean)
 
 @Composable
 fun AppLayout() {
@@ -58,8 +38,19 @@ fun AppLayout() {
         bottomBar = { Navbar() }
     ) { contentPadding ->
         Column(modifier = Modifier.padding(contentPadding)) {
-            // ScrollableList()
-            MaterialList()
+            // Aggiungi il saldo totale , provvisorio da aggiungere la card..
+            TotalBalance("3,470.00 €")
+            // Aggiungi la lista degli account
+            val accounts = listOf(
+                Account("N26", "420.00 €", Icons.Filled.ShoppingCart, true),
+                Account("Hype", "360.00 €", Icons.Filled.AccountBalance, true),
+                Account("Wallet", "100.00 €", Icons.Filled.ShoppingCart, false),
+                Account("Deutsche Bank", "1,500.00 €", Icons.Filled.AccountBalance, false),
+                Account("Investments", "940.00 €", Icons.Filled.Leaderboard, false),
+                Account("Safe", "400.00 €", Icons.Filled.ShoppingCart, false),
+                Account("Revolut", "200.00 €", Icons.Filled.AccountBalance, false)
+            )
+            AccountList(accounts)
         }
     }
 }
@@ -134,30 +125,127 @@ fun MaterialListItem(item: String, modifier: Modifier = Modifier) {
 @Composable
 fun Navbar() {
     var selectedItem by remember { mutableIntStateOf(0) }
+
     NavigationBar {
         NavigationBarItem(
-            icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
+            icon = {
+                Icon(
+                    imageVector = if (selectedItem == 0) Icons.Filled.Home else Icons.Rounded.Home,
+                    contentDescription = "Home"
+                )
+            },
             label = { Text("Home") },
             selected = selectedItem == 0,
             onClick = { selectedItem = 0 }
         )
         NavigationBarItem(
-            icon = { Icon(Icons.Filled.Assignment, contentDescription = "Transactions") },
+            icon = {
+                Icon(
+                    imageVector = if (selectedItem == 1) Icons.Filled.Assignment else Icons.Rounded.Assignment,
+                    contentDescription = "Transactions"
+                )
+            },
             label = { Text("Transactions") },
             selected = selectedItem == 1,
             onClick = { selectedItem = 1 }
         )
         NavigationBarItem(
-            icon = { Icon(Icons.Filled.Schedule, contentDescription = "Recurrents") },
-            label = { Text("Home") },
+            icon = {
+                Icon(
+                    imageVector = if (selectedItem == 2) Icons.Filled.Schedule else Icons.Rounded.Schedule,
+                    contentDescription = "Recurrents"
+                )
+            },
+            label = { Text("Recurrents") },
             selected = selectedItem == 2,
             onClick = { selectedItem = 2 }
         )
         NavigationBarItem(
-            icon = { Icon(Icons.Filled.Leaderboard, contentDescription = "Stats") },
+            icon = {
+                Icon(
+                    imageVector = if (selectedItem == 3) Icons.Filled.Leaderboard else Icons.Rounded.Leaderboard,
+                    contentDescription = "Stats"
+                )
+            },
             label = { Text("Stats") },
             selected = selectedItem == 3,
             onClick = { selectedItem = 3 }
         )
+    }
+}
+
+
+@Composable
+fun TotalBalance(balance: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp)
+    ) {
+        Text(
+            text = "Total Balance",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+        Text(
+            text = balance,
+            style = MaterialTheme.typography.displaySmall,
+            color = MaterialTheme.colorScheme.onBackground,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun AccountList(accounts: List<Account>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        items(accounts) { account ->
+            AccountItem(account)
+        }
+    }
+}
+
+@Composable
+fun AccountItem(account: Account) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Icon(
+            imageVector = account.icon,
+            contentDescription = account.name,
+            modifier = Modifier.size(40.dp)
+        )
+        Spacer(Modifier.size(16.dp))
+        Column {
+            Text(
+                text = account.name,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = account.balance,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        if (account.isFavorite) {
+            Icon(
+                imageVector = Icons.Filled.Favorite,
+                contentDescription = "Favorite",
+                modifier = Modifier.size(24.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
     }
 }
