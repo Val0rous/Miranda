526285ba61b41152d26dc2f174baaa0f64efb747
Francesco Valentini <fv26012001@gmail.com>
2024-08-30 02:36:52 +0200

    Improve MapView

--- app/src/main/java/com/cashflowtracker/miranda/ui/composables/MapScreen.kt
+++ app/src/main/java/com/cashflowtracker/miranda/ui/composables/MapScreen.kt
@@ -1,23 +1,43 @@
 package com.cashflowtracker.miranda.ui.composables
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.BitmapFactory
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.cashflowtracker.miranda.data.database.Transaction
+import com.cashflowtracker.miranda.ui.screens.ViewTransaction
+import com.cashflowtracker.miranda.ui.theme.CustomColors
 import com.cashflowtracker.miranda.utils.Coordinates
 import com.cashflowtracker.miranda.utils.createRoundedMarkerIcon
 import com.cashflowtracker.miranda.utils.iconFactory
@@ -29,6 +49,8 @@ import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun MapScreen(
@@ -76,8 +98,12 @@ fun MapScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FullScreenMapView(transactions: List<Transaction>) {
+    var showDialog by remember { mutableStateOf(false) }
+    var openTransaction by remember { mutableStateOf<Transaction?>(null) }
+    val context = LocalContext.current
     val initialCoordinates = transactions.firstOrNull()?.location?.split(", ", limit = 2)?.let {
         if (it.size == 2) {
             LatLng(it[0].toDouble(), it[1].toDouble())
@@ -127,9 +153,102 @@ fun FullScreenMapView(transactions: List<Transaction>) {
                     ),
                     title = transaction.comment ?: transaction.location,
                     snippet = transaction.type,
-                    icon = markerIcon
+                    icon = markerIcon,
+                    onClick = {
+                        showDialog = true
+                        openTransaction = transaction
+                        false // Return false to keep default behavior, true to prevent default behavior
+                    }
                 )
             }
+        }
+    }
+
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+    if (showDialog) {
+        ModalBottomSheet(
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+            onDismissRequest = {
+                showDialog = false
+                openTransaction = null
+            }
+        ) {
+            ListItem(
+                leadingContent = {
+                    IconWithBackground(
+                        icon = iconFactory(
+                            openTransaction!!.type,
+                            openTransaction!!.source,
+                            openTransaction!!.destination
+                        ),
+                        iconSize = 24.dp,
+                        iconColor = CustomColors.current.icon,
+                        backgroundSize = 40.dp,
+                        backgroundColor = when (openTransaction!!.type) {
+                            "Output" -> CustomColors.current.surfaceTintRed
+                            "Input" -> CustomColors.current.surfaceTintGreen
+                            else -> CustomColors.current.surfaceTintBlue
+                        }
+                    )
+                },
+                overlineContent = {
+                    Text(
+                        text = ZonedDateTime.parse(
+                            openTransaction!!.dateTime,
+                            DateTimeFormatter.ISO_ZONED_DATE_TIME
+                        ).format(DateTimeFormatter.ofPattern("yyyy-MM-dd  ·  HH:mm")),
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                },
+                headlineContent = {
+                    Text(
+                        text = openTransaction!!.comment ?: "",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                },
+                trailingContent = {
+                    Text(
+                        text = when (openTransaction!!.type) {
+                            "Output" -> if (openTransaction!!.amount != 0.0) "-%.2f €" else "%.2f €"
+                            "Input" -> if (openTransaction!!.amount != 0.0) "+%.2f €" else "%.2f €"
+                            else -> "%.2f €"
+                        }.format(openTransaction!!.amount),
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onSurface
+//                                when (transaction.type) {
+//                                    "Output" -> CustomColors.current.surfaceTintRed
+//                                    "Input" -> CustomColors.current.surfaceTintGreen
+//                                    else -> CustomColors.current.surfaceTintBlue
+//                                }
+                    )
+                },
+                modifier = Modifier
+                    .padding(top = 0.dp, bottom = 24.dp, start = 24.dp, end = 24.dp)
+                    .border(
+                        1.dp,
+                        MaterialTheme.colorScheme.outlineVariant,
+                        RoundedCornerShape(7.dp)
+                    )
+                    .clickable {
+                        val intent = Intent(context, ViewTransaction::class.java)
+                        intent.putExtra("transactionId", openTransaction!!.transactionId.toString())
+                        context.startActivity(intent)
+                    }
+            )
+
+            val color = MaterialTheme.colorScheme.surfaceContainerLow
+            SideEffect {
+                window.navigationBarColor = color.toArgb()
+            }
+        }
+    } else {
+        val color = Color.Transparent
+        SideEffect {
+            window.navigationBarColor = color.toArgb()
         }
     }
 }
--- app/src/main/java/com/cashflowtracker/miranda/ui/screens/MapView.kt
+++ app/src/main/java/com/cashflowtracker/miranda/ui/screens/MapView.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -42,7 +43,7 @@ class MapView : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
                         TopAppBar(
-                            title = { },
+                            title = { Text("Map view") },
                             navigationIcon = {
                                 IconButton(
                                     onClick = { finish() },
