f795d037256d27b94c12d23dbcabff673ec5ef2e
Francesco Valentini <fv26012001@gmail.com>
2024-11-12 16:10:27 +0100

    Fix splash screen color: now following system theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.cashflowtracker.miranda"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.cashflowtracker.miranda"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -53,14 +53,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-    implementation("androidx.activity:activity-compose:1.9.1")
-    implementation(platform("androidx.compose:compose-bom:2024.08.00"))
-    implementation("androidx.lifecycle:lifecycle-runtime-compose-android:2.8.4")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation("androidx.lifecycle:lifecycle-runtime-compose-android:2.8.7")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.media3:media3-effect:1.4.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
 
@@ -69,11 +69,12 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.compose.material:material-icons-extended:1.6.8")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.1")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation("androidx.media3:media3-test-utils:1.4.1")
     //implementation("androidx.room:room-ktx:2.6.1")
     val roomVersion = "2.6.1"
@@ -103,8 +104,8 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
-    implementation("com.google.android.libraries.places:places:3.5.0")
-    implementation("com.google.maps.android:maps-compose:2.5.0")
+    implementation("com.google.android.libraries.places:places:4.0.0")
+    implementation("com.google.maps.android:maps-compose:4.3.3")
 
     // UI Tests
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
@@ -115,7 +116,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // Google Material Symbols
-    implementation("androidx.compose.material:material-icons-extended:1.6.8")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
 
     // Google Maps
     implementation("com.google.maps.android:maps-compose:4.3.3")
